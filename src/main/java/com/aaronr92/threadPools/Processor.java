97f3ccf2b36c384a950f20a731933590eaefacd9
Anton <86726028+AaronR92@users.noreply.github.com>
2022-12-31 19:58:52 +0300

    Thread pools

---
+++ src/main/java/com/aaronr92/threadPools/Processor.java
@@ -0,0 +1,26 @@
+package com.aaronr92.threadPools;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Processor implements Runnable {
+    private int id;
+    Processor(int id) {
+        this.id = id;
+        System.out.printf("[%d] Created%n", id);
+    }
+    @Override
+    public void run() {
+        System.out.printf("[%d] Starting at %s%n", id, LocalDateTime.now());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("[%d] Completed at %s%n", id, LocalDateTime.now());
+    }
+
+    public int getId() {
+        return id;
+    }
+}
