717789c17f7028f879c7c0b93f4070f4d545277c
Anton <86726028+AaronR92@users.noreply.github.com>
2023-01-02 12:47:23 +0300

    Condition added
    Wait and notify functional

--- src/main/java/com/aaronr92/SynchronizedKeyword.java
+++ src/main/java/com/aaronr92/SynchronizedKeyword.java
@@ -1,11 +1,11 @@
 package com.aaronr92;
 
 public class SynchronizedKeyword {
-    // A synchronized method must be outside runnable
     public static int value = 0;
+
     public static void main(String[] args) throws InterruptedException {
-        IncrementThread t1 = new IncrementThread();
-        IncrementThread t2 = new IncrementThread();
+        Thread t1 = new IncrementThread();
+        Thread t2 = new IncrementThread();
 
         t1.start();
         t2.start();
@@ -21,11 +21,12 @@ class IncrementThread extends Thread {
     /**
      * A synchronized method must be outside runnable
      */
-    public static synchronized void increment() {
+    private static synchronized void increment() {
         SynchronizedKeyword.value++;
     }
+
     @Override
-    public synchronized void run() {
+    public void run() {
         for (int i = 0; i < 10_000; i++) {
             increment();
         }
--- src/main/java/com/aaronr92/reEntrantLocks/Runner.java
+++ src/main/java/com/aaronr92/reEntrantLocks/Runner.java
@@ -1,37 +1,55 @@
 package com.aaronr92.reEntrantLocks;
 
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Runner {
     private int count = 0;
     private final Lock lock = new ReentrantLock();
+    private final Condition cond = lock.newCondition();
 
-    private void increment() {
-        for (int i = 0; i < 10_000; i++) {
+    private void increment() throws InterruptedException {
+        for (int i = 0; i < 1_000; i++) {
             count++;
+            Thread.sleep(1);
         }
     }
 
     public void firstThread() throws InterruptedException {
         lock.lock();    // disadvantage - when code inside will throw
                         // an exception, unlock() will never be called
+        System.out.println("Waiting...");
+        cond.await();   // executing wait method and releasing the lock
+        System.out.println("Waking up...");
+
         try {
             increment();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            System.out.println(Thread.currentThread().getName() + " unlocked");
             lock.unlock();
         }
     }
 
     public void secondThread() throws InterruptedException {
         lock.lock();
+        Thread.sleep(1000);
+
+        System.out.println("Press return key to continue...");
+        new Scanner(System.in).nextLine();
+        System.out.println("Got return!");
+
+        cond.signal();
+
         try {
             increment();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            System.out.println(Thread.currentThread().getName() + " unlocked");
             lock.unlock();
         }
     }
